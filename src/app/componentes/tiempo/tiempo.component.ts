30488d039fb5cc233c1161e3e172b6d6fc5328e6
cristian <cra.bustamante@alumnos.duoc.cl>
2021-09-11 13:01:32 -0300

    uso del formulario

--- src/app/componentes/tiempo/tiempo.component.ts
+++ src/app/componentes/tiempo/tiempo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-tiempo',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TiempoComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup;
+
+  constructor(private formbuilder:FormBuilder) {
+
+    this.iniciaFormulario();
+
+  }
 
   ngOnInit(): void {
+  }
+
+  /*
+  * Metodo que inicia el fomulario con valores por defecto
+  */
+
+  iniciaFormulario(){
+    this.formulario = this.formbuilder.group({
+      ciudad:['Santiago'],
+      codigo:['CL']
+    })
+  }
+
+
+  consultar(){
+    console.log("datos ",this.formulario.value);
+
   }
 
 }
